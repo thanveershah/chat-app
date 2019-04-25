ed50d40552eaf9dd88aa637ab792c11572eb2744
bharathi <thanveer.bharathi@gmail.com>
2019-04-25 17:21:59 +0530

    Fixed design

--- bleh.js
+++ bleh.js
@@ -4,64 +4,62 @@ const cookieParser = require("cookie-parser");
 const pathname = require("path");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-
+const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
+// app.use(express.static(pathname.join(__dirname, "chatlog.html")));
 
 app.get("/", (req, res) => {
   res.sendFile(pathname.join(__dirname, "chat.html"));
 });
 
-io.on("connection", client => {
-  console.log("Client Connected");
+// io.on("connection", client => {
+//   console.log("Client Connected");
 
-  client.on("join", data => {
-    console.log(data);
-  });
+//   client.on("join", data => {
+//     console.log(data);
+//   });
 
-  client.on("messages", data => {
-    client.emit("broad", data);
-    client.broadcast.emit("broad", data);
-  });
+//   client.on("messages", data => {
+//     client.emit("broad", data);
+//     client.broadcast.emit("broad", data);
+//   });
 
-  client.on("isTyping", () => {
-    client.broadcast.emit("typing", "Typing");
-  });
-  client.on("notTyping", () => {
-    client.broadcast.emit("nottyping", "Stopped");
-  });
-});
-
-
-
-
+//   client.on("isTyping", () => {
+//     client.broadcast.emit("typing", "Typing");
+//   });
+//   client.on("notTyping", () => {
+//     client.broadcast.emit("nottyping", "Stopped");
+//   });
+// });
 
 users = [];
-io.on('connection', function(socket) {
-   console.log('A user connected');
-   socket.on('setUsername', function(data) {
-      console.log(data);
-      
-      if(users.indexOf(data) > -1) {
-         socket.emit('userExists', data + ' username is taken! Try some other username.');
-      } else {
-         users.push(data);
-         socket.emit('userSet', {username: data});
-      }
-   });
-   
-   socket.on('msg', function(data) {
-      io.sockets.emit('newmsg', data);
-   })
+io.on("connection", function(socket) {
+  // var destination = "chatlog.html";
+  console.log("A user connected");
+  socket.on("setUsername", function(data) {
+    console.log(data);
 
-   socket.on("isTyping", () => {
+    if (users.indexOf(data) > -1) {
+      socket.emit("userExists", data + " username is taken!");
+    } else {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+      // socket.emit("redirect", destination);
+    }
+  });
+
+  socket.on("msg", function(data) {
+    io.sockets.emit("newmsg", data);
+  });
+
+  socket.on("isTyping", () => {
     client.broadcast.emit("typing", "Typing");
   });
   socket.on("notTyping", () => {
     client.broadcast.emit("nottyping", "Stopped");
   });
 });
-
 
 http.listen(PORT, () => console.log(PORT));
