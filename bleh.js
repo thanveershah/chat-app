ffd19f262dd82d001255667cd592d81b127bfb27
bharathi <thanveer.bharathi@gmail.com>
2019-04-26 12:02:17 +0530

    Join and Lef

--- bleh.js
+++ bleh.js
@@ -3,9 +3,7 @@ const app = express();
 const pathname = require("path");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const fs = require("fs");
 const PORT = process.env.PORT || 5000;
-const cookieParser = require("cookie-parser");
 
 app.get("/", (req, res) => {
   res.sendFile(pathname.join(__dirname, "chat.html"));
@@ -33,28 +31,22 @@ app.get("/", (req, res) => {
 
 var users = [];
 io.on("connection", function(socket) {
-  console.log("A user connected");
   socket.on("setUsername", function(data) {
-    console.log(data);
-
     if (users.indexOf(data) > -1) {
       socket.emit("userExists", data + " username is taken!");
     } else {
       users.push(data);
       socket.emit("userSet", { username: data });
-      socket.emit("joined", data + " has joined");
+      socket.broadcast.emit("joined", data + " has joined");
     }
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("left", data + " has left");
+    });
   });
 
   socket.on("msg", data => {
     io.sockets.emit("newmsg", data);
-    socket.on("isTyping", () => {
-      socket.broadcast.emit("typing", "Typing");
-    });
-    socket.on("notTyping", () => {
-      socket.broadcast.emit("nottyping", "Stopped");
-    });
   });
 });
 
-http.listen(PORT, () => console.log(PORT));
+http.listen(PORT, () => console.log("Serever Started in " + PORT));
