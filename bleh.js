c9923d90d3f98fbcaa02b3f6ccd60cc952effcb5
bharathi <thanveer.bharathi@gmail.com>
2019-04-29 19:15:49 +0530

    Fixed Mobile Notification

--- bleh.js
+++ bleh.js
@@ -43,7 +43,7 @@ io.on("connection", socket => {
     } else {
       users.push(data);
       socket.emit("userSet", { username: data });
-      socket.broadcast.emit("joined", data + " has joined");
+      socket.broadcast.emit("joined", data);
       //    socket.broadcast.emit("notify everyone", {
       //   user: data.user,
       //   comment: data.message
