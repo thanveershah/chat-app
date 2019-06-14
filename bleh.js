2903cad3afd8d0d842813699015899b321126a57
bharathi <thanveer.bharathi@gmail.com>
2019-06-14 16:23:26 +0530

    dsd

--- bleh.js
+++ bleh.js
@@ -3,14 +3,6 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const pathname = require("path");
-const MongoClient = require("mongodb").MongoClient;
-
-const url = "mongodb://localhost:27017/mychatdb";
-
-//Express Way
-// const server = app.listen(5000)
-// const io = require("socket.io")(server);
-//
 
 const PORT = process.env.PORT || 5000;
 
@@ -38,7 +30,7 @@ app.get("/", (req, res) => {
 //   });
 // });
 
-const users = [];
+const users = []
 io.on("connection", socket => {
   socket.on("setUsername", data => {
     if (users.indexOf(data) > -1) {
@@ -47,17 +39,6 @@ io.on("connection", socket => {
       users.push(data);
       socket.emit("userSet", { username: data });
       socket.broadcast.emit("joined", data);
-      MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("mychatdb");
-        dbo
-          .collection("chats")
-          .find({}, { projection: { _id: 0, username: 1, message: 1 } })
-          .toArray((err, result) => {
-            if (err) throw err;
-            db.close();
-          });
-      });
     }
     socket.on("disconnect", () => {
       socket.broadcast.emit("left", data + " has left");
@@ -70,77 +51,9 @@ io.on("connection", socket => {
       user: data.user,
       comment: data.message
     });
-    MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
-      if (err) throw err;
-      const dbo = db.db("mychatdb");
-      const myobj = { username: data.user, message: data.message };
-      dbo.collection("chats").insertOne(myobj, (err, res) => {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    });
   });
 
-  //Total Count
-  let counts = 0;
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-    if (err) throw err;
-    const dbo = db.db("mychatdb");
-    let n = dbo.collection("chats").find();
-    n.count((err, data) => {
-      counts = data;
-    });
-  });
-
-  //Top to Bottom
-  app.get("/data/:limit", (req, res) => {
-    let start = parseInt(req.params.limit);
-    let end = 15 + start;
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      const dbo = db.db("mychatdb");
-      dbo
-        .collection("chats")
-        .find({}, { projection: { _id: 0, username: 1, message: 1 } })
-        .skip(start)
-        .limit(end)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res.json(result);
-          db.close();
-        });
-    });
-  });
-
-  //Bottom to Top;
-  // app.get("/data/:start/:end", (req, res) => {
-  //   let start = parseInt(req.params.start);
-  //   let end = parseInt(req.params.end);
-  //   start = counts - start;
-  //   end = counts - end;
-  //   if (start >= 0) {
-  //     MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-  //       if (err) throw err;
-  //       const dbo = db.db("mychatdb");
-  //       dbo
-  //         .collection("chats")
-  //         .find({}, { projection: { _id: 0, username: 1, message: 1 } })
-  //         .skip(start)
-  //         .limit(end)
-  //         .toArray((err, result) => {
-  //           if (err) throw err;
-  //           res.json(result);
-  //           db.close();
-  //         });
-  //     });
-  //   } else {
-  //     return false;
-  //   }
-  //   console.log(counts);
-  //   console.log(end);
-  //   console.log(start);
-  // });
+  app.get("/data", (req, res) => {});
 
   //Send Message
 
